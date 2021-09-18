d08ec2ac8701ecca78c3cf88b306367f2615e91a
Abdurrehman <79513307+AbdurrehmanMuhammadIbrahim@users.noreply.github.com>
2021-09-18 23:05:22 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+import img1 from "../src/images/img.jpg"
 import Home from "./components/home/home";
 import About from "./components/about/about";
 import Services from "./components/services/services";
@@ -18,7 +21,9 @@ function App() {
  
       <Router>
         <div>
-          <ul>
+
+          {/* ROUTER WORKINGS */}
+          {/* <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
@@ -30,7 +35,61 @@ function App() {
             </li>
           </ul>
   
-          <hr />
+          <hr /> */}
+
+
+
+
+          <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        
+          <Link class="nav-link active" to="/">Home</Link>
+          {/* <Link to="/services">Services</Link> */}
+        </li>
+        <li class="nav-item">
+        <Link class="nav-link active" to="/services">Services</Link>
+        </li>
+        {/* <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> */}
+        <li class="nav-item">
+         
+         <Link  class="nav-link " to="/about">About</Link>
+     
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
           <Switch>
           <Route exact path="/">
             <Home />
@@ -44,8 +103,32 @@ function App() {
         </Switch>
       </div>
     </Router>
-  
-
+    <div class="card-group">
+  <div class="card">
+    <img src={img1} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src={img1} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src={img1} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
 
      
     </div>
